8ae5be124b4e8691d357fb92117ae707bcdc6f76
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2017-07-02 01:17:39 +0530

    added database

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -12,7 +12,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'insert_your_database_url_here';
+var mongoDB = process.env.dburl; //environment variable containing url
 mongoose.connect(mongoDB);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
